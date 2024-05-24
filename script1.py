90b04e84d8f89aaa9bcac6947ea14994d190eb1f
justinehlert <justin.ehlert@flemingcollege.ca>
2024-05-24 16:41:22 -0400

    Added new feature and comments
    -Added comments explaining why there are no print statements in this script
    -Added amazing new feature

--- script1.py
+++ script1.py
@@ -1,1 +1,5 @@
 #Prints the second line of Zen of Python
+#I see now in the lab there probably should've been two print statements
+#I only had one so now there is nothing here... whoops
+
+#This is an amazing new feature!
